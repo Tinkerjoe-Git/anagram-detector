be0f5f001acb7356734b029d81d81f275cde3393
Tinkergit <nicholas.stagno@gmail.com>
2021-02-08 19:00:01 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
-# Your code goes here!
+
+
+class Anagram
+    attr_accessor :bananagram
+
+
+    def initialize(bananagram)
+        @bananagram=bananagram
+    end
+
+
+    def match(word)
+        word.select do |word|
+            izzit?(word)
+        end
+    end
+        #anagram=[]
+        #array.select do |w| 
+            #anagram << w if w.chars.sort.join== bananagram.chars.sort.join
+        #$end
+        #anagram
+    
+
+    def izzit?(word)
+        word.chars.sort==@bananagram.chars.sort
+    end    
+end
+
